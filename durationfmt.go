713d3a38da28dbdd8a06cf25e09edd871cd9b2a2
David Scholberg <recombinant.vector@gmail.com>
2017-01-22 00:03:36 -0500

    code cleanup: modularized into functions, added documentation

--- durationfmt.go
+++ durationfmt.go
@@ -1,3 +1,5 @@
+// durationfmt provides a function to format durations according to a format
+// string.
 package durationfmt
 
 import (
@@ -9,19 +11,13 @@ const Day = 24 * time.Hour
 const Week = 7 * Day
 const Year = 365 * Day
 
-type duration struct {
+// durationUnit represets a possible duration unit. A durationUnit object
+// contains the divisor that the duration unit uses as well as if that duration
+// unit is present in the duration format.
+type durationUnit struct {
 	Present    bool
 	DurDivisor time.Duration
 }
-
-//type durationUnits struct {
-//	Years   duration
-//	Weeks   duration
-//	Days    duration
-//	Hours   duration
-//	Minutes duration
-//	Seconds duration
-//}
 
 // Format formats the given duration according to the given format string.
 // %y - # of years
@@ -31,28 +27,61 @@ type duration struct {
 // %m - # of minutes
 // %s - # of seconds
 // %% - print a percent sign
+// You can place a 0 before the h, m, and s modifiers to zeropad those values to
+// two digits. Zeropadding is undefined for the other modifiers.
 func Format(dur time.Duration, fmtStr string) (string, error) {
-	var durationUnits = map[string]*duration{
-		"y": &duration{
+	var durUnits = map[string]*durationUnit{
+		"y": &durationUnit{
 			DurDivisor: Year,
 		},
-		"w": &duration{
+		"w": &durationUnit{
 			DurDivisor: Week,
 		},
-		"d": &duration{
+		"d": &durationUnit{
 			DurDivisor: Day,
 		},
-		"h": &duration{
+		"h": &durationUnit{
 			DurDivisor: time.Hour,
 		},
-		"m": &duration{
+		"m": &durationUnit{
 			DurDivisor: time.Minute,
 		},
-		"s": &duration{
+		"s": &durationUnit{
 			DurDivisor: time.Second,
 		},
 	}
 
+	sprintfFmt, durCount, err := parseFmtStr(fmtStr, durUnits)
+	if err != nil {
+		return "", err
+	}
+
+	durArray := make([]interface{}, durCount)
+	calculateDurUnits(dur, durArray, durUnits)
+
+	return fmt.Sprintf(sprintfFmt, durArray...), nil
+}
+
+// calculateDurUnits takes a duration and breaks it up into its constituent
+// duration unit values.
+func calculateDurUnits(dur time.Duration, durArray []interface{}, durUnits map[string]*durationUnit) {
+	remainingDur := dur
+	durCount := 0
+	for _, c := range "ywdhms" {
+		durChar := string(c)
+		if durUnits[durChar].Present {
+			durArray[durCount] = remainingDur / durUnits[durChar].DurDivisor
+			remainingDur = remainingDur % durUnits[durChar].DurDivisor
+			durCount++
+		}
+	}
+}
+
+// parseFmtStr parses the given duration format string into its constituent
+// units.
+// parseFmtStr returns a format string that can be passed to fmt.Sprintf and a
+// count of how many duration units are in the format string.
+func parseFmtStr(fmtStr string, durUnits map[string]*durationUnit) (string, int, error) {
 	modifier, zeropad := false, false
 	sprintfFmt := ""
 	durCount := 0
@@ -66,8 +95,8 @@ func Format(dur time.Duration, fmtStr string) (string, error) {
 			}
 			continue
 		}
-		if _, ok := durationUnits[fmtChar]; ok {
-			durationUnits[fmtChar].Present = true
+		if _, ok := durUnits[fmtChar]; ok {
+			durUnits[fmtChar].Present = true
 			durCount++
 			if zeropad {
 				sprintfFmt += "%02d"
@@ -83,23 +112,10 @@ func Format(dur time.Duration, fmtStr string) (string, error) {
 			case "%":
 				sprintfFmt += "%%"
 			default:
-				return "", fmt.Errorf("incorrect duration modifier")
+				return "", durCount, fmt.Errorf("incorrect duration modifier")
 			}
 		}
 		modifier = false
 	}
-
-	remainingDur := dur
-	durationArray := make([]interface{}, durCount)
-	durCount = 0
-	for _, c := range "ywdhms" {
-		durChar := string(c)
-		if durationUnits[durChar].Present {
-			durationArray[durCount] = remainingDur / durationUnits[durChar].DurDivisor
-			remainingDur = remainingDur % durationUnits[durChar].DurDivisor
-			durCount++
-		}
-	}
-
-	return fmt.Sprintf(sprintfFmt, durationArray...), nil
+	return sprintfFmt, durCount, nil
 }
